30a9f0796e7fe942ab0a28ffe476153bc1863fd9
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-11-07 02:24:18 +0300

    Added .env

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "d09b045f520d6923a6ecfc7a8be12d47";
+const API_KEY = process.env.REACT_APP_API_KEY
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
