c391f976bce4fb9132ede9e21d65b158c2290ae2
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-11-07 01:27:22 +0300

    Weather App With React

---
+++ src/components/TopButtons.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function TopButtons({ setQuery }) {
+    const cities = [
+        { id: 1, title: "London" },
+        { id: 2, title: "Istanbul" },
+        { id: 3, title: "Tokyo" },
+        { id: 4, title: "Berlin" },
+        { id: 5, title: "Baku" },
+    ];
+
+    return (
+        <div className="flex items-center justify-around my-6">
+            {cities.map((city) => (
+                <button
+                    key={city.id}
+                    className="text-white text-lg font-medium"
+                    onClick={() => setQuery({ q: city.title })}
+                >
+                    {city.title}
+                </button>
+            ))}
+        </div>
+    );
+}
+
+export default TopButtons;
